21810cfcc190958726ec80aa6837904e1cfc55d6
marocks31 <mollyarisley@gmail.com>
2023-01-04 16:25:27 -0600

    hardcoding

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Hello WORLD!
-      </p>
+        <header>
+        {/* when you use a pound sign it will link to elements with that id on the page */}
+          <a href="#">Home</a> | <a href="#crafts-index">All crafts</a>
+        </header>
+        <div id="crafts-new">
+          <h1>New craft</h1>
+          <form>  
+            <div>
+              Name: <input type="text"/>
+            </div>
+            <div>
+              Description: <input type="text"/>
+            </div>
+            <div>
+              Materials: <input type="text"/>
+            </div>
+            <div>
+              Image: <input type="text"/>
+            </div>
+            <button>Create craft</button>
+          </form>
+        </div>
+        <div id="crafts-index">
+          <h1>All crafts</h1>
+          <div className="crafts">
+            <h1>Potato Dolls</h1>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq3h_G3rNfaCTvdqR-WXLnHWDks4qGJ9VrHw&usqp=CAU" alt="picture of button card"/>
+            <p>Make a doll out of potatoes</p>
+            <button>More info</button>
+          </div>
+          <div className="crafts">
+            <h1>Painted rocks</h1>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfegGYg48PBQAGXtDmk8SqoetQWeY5EO4Ruh4-zBjzq61KPbv13-c460giPj4R6niLolQ&usqp=CAU" alt="picture of button card"/>
+            <p>Paint rocks</p>
+            <button>More info</button>
+          </div>
+        </div>
+        <footer>
+          copyright 2023!
+        </footer>
     </div>
-  )
+  );
 }
 
 export default App
