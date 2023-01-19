bb28196e26057c98d5524693b1088171e668721c
marocks31 <mollyarisley@gmail.com>
2023-01-19 14:40:19 -0600

    pass objects through index and home components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function Header() {
   return (
     <div>
@@ -32,30 +34,21 @@ function CraftsNew() {
   )
 }
 
-function CraftsIndex() {
+function CraftsIndex(props) {
+  console.log(props.myCrafts);
   return (
     <div id="crafts-index">
       <h1>All crafts</h1>
-      <div className="crafts">
-        <h1>Potato Dolls</h1>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq3h_G3rNfaCTvdqR-WXLnHWDks4qGJ9VrHw&usqp=CAU" alt="picture potato with a face and outfit"/>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <button>More info</button>
+      {props.myCrafts.map((craft) => (
+      <div key={craft.id} className="crafts">
+        <h2>{craft.name}</h2> 
+        <img src={craft.image} alt="diy craft" />
+        <h5>Description: {craft.description}</h5>
+        <h5>Materials: {craft.materials}</h5>
       </div>
-      <div className="crafts">
-        <h1>Painted rocks</h1>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfegGYg48PBQAGXtDmk8SqoetQWeY5EO4Ruh4-zBjzq61KPbv13-c460giPj4R6niLolQ&usqp=CAU" alt="picture of painted rocks"/>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <button>More info</button>
-      </div>
-      <div className="crafts">
-        <h1>Shrinky Dink Crafts</h1>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHw8-giefeQTQSAXwD40aDxw4gixdEf9XrAg&usqp=CAU" alt="Shrinky Dink"/>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <button>More info</button>
-      </div>
+      ))}
     </div>
-  )
+  );
 }
 
 function Footer() {
@@ -66,10 +59,32 @@ function Footer() {
   )
 }
 function Home() {
+  // crafts is variable, setCrafts is method to change variable
+  // useState hook keeps variable reactive and updates DOM if craft is added or deleted
+  const [crafts, setCrafts] = useState([
+    {
+      name: "Potato Dolls",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      materials: "potatoes",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq3h_G3rNfaCTvdqR-WXLnHWDks4qGJ9VrHw&usqp=CAU",
+    },
+    {
+      name: "Painted rocks",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      materials: "rocks and paint",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfegGYg48PBQAGXtDmk8SqoetQWeY5EO4Ruh4-zBjzq61KPbv13-c460giPj4R6niLolQ&usqp=CAU"
+    },
+    {
+      name: "Shrinky Dink Crafts",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      materials: "markers, oven, shrinky dink paper",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHw8-giefeQTQSAXwD40aDxw4gixdEf9XrAg&usqp=CAU",
+    },
+    ]);
   return (
     <div>
       <CraftsNew/>
-      <CraftsIndex/>
+      <CraftsIndex myCrafts={crafts}/>
     </div>
   )
 }
