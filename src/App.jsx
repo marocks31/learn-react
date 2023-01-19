2daa74796e139f61107fc40daccfe7f560229c39
marocks31 <mollyarisley@gmail.com>
2023-01-19 17:28:13 -0600

    connect to backend using axios request, setCrafts function to change state of the variable crafts

--- src/App.jsx
+++ src/App.jsx
@@ -64,10 +64,20 @@ function Home() {
   // crafts is variable, setCrafts is method to change variable
   // useState hook keeps variable reactive and updates DOM if craft is added or deleted
   const [crafts, setCrafts] = useState([]);
+  //function that grabs data
+  const handleIndexCrafts = () => {
+    //.then ensures that you get the data before moving on
+    axios.get("http://localhost:3000/crafts.json").then
+    ((response) => {
+      console.log(response.data);
+      setCrafts(response.data);
+    });
+  };
   return (
     <div>
       <CraftsNew/>
       <CraftsIndex myCrafts={crafts}/>
+      <button onClick={handleIndexCrafts}>Load crafts</button>
     </div>
   )
 }
