a37c41266dd09ef0c2a9867db9652b2c64ca06a7
marocks31 <mollyarisley@gmail.com>
2023-02-01 19:08:49 -0600

    finish submit action on signup form

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,9 +1,16 @@
 import axios from "axios";
+
 export function Signup() {
   const handleSubmit = (event) => {
-    event.preventDefault();
-    const params = new FormData(event.target);
-    axios.post("http://localhost:3000/users", params).then(response => { console.log(response)});
+    event.preventDefault(); //prevents from refreshing or moving to different page
+    const params = new FormData(event.target); //grab params/data from the form
+    axios.post("http://localhost:3000/users", params) //how to submit params, pass params as argument to back end 
+    .then((response) => { console.log(response); //.then because we don't know how long it will take, console log the response should show errors or 'user sucessfully created'
+    event.target.reset(); //reset form after 'signup' button click, reset is built in JS function
+    })
+    .catch((errors) => { //catch errors and print to console
+      console.log(errors.response.data.errors);
+    });
   };
 
   return (
