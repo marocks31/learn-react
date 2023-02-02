d345018184eacebe5e8d4da1b1887065cff5ee94
marocks31 <mollyarisley@gmail.com>
2023-02-01 19:43:44 -0600

    display error messages for sign up action

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,8 +1,11 @@
 import axios from "axios";
+import { useState } from "react";
 
 export function Login() {
+  const [errors, setErrors] = useState([]); //create a variable to hold errors
   const handleSubmit = (event) => {
     event.preventDefault(); //prevents from refreshing or moving to different page
+    setErrors([]);
     const params = new FormData(event.target); //grab params/data from the form, FormData is a vanilla JS function 
     axios.post("http://localhost:3000/sessions", params) //how to submit params, pass params as argument to back end 
     .then((response) => { console.log(response); //.then because we don't know how long it will take, console log the response should show errors or 'user sucessfully created'
@@ -10,12 +13,18 @@ export function Login() {
     })
     .catch((errors) => { //catch errors and print to console
       console.log(errors.response.data.errors);
+      setErrors(error.response.data.errors); //display errors to user
     });
   };
 
   return (
     <div id="login">
     <h1>Login</h1>
+    <ul>
+      {errors.map((error) => (
+        <li key={error}>{error}</li>
+      ))}
+    </ul>
       <form onSubmit={handleSubmit}>
         <div>
           Email: <input name="email" type="email" className="form-control"/>
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,21 +1,32 @@
 import axios from "axios";
+import { useState } from "react";
 
 export function Signup() {
+  const [errors, setErrors] = useState([]); //create a variable to hold errors
+
   const handleSubmit = (event) => {
     event.preventDefault(); //prevents from refreshing or moving to different page
+    setErrors([]);
     const params = new FormData(event.target); //grab params/data from the form, FormData is a vanilla JS function 
     axios.post("http://localhost:3000/users", params) //how to submit params, pass params as argument to back end 
-    .then((response) => { console.log(response); //.then because we don't know how long it will take, console log the response should show errors or 'user sucessfully created'
+    .then((response) => { console.log(response.data); //.then because we don't know how long it will take, console log the response should show errors or 'user sucessfully created'
     event.target.reset(); //reset form after 'signup' button click, reset is built in JS function
+    window.location.href = '/' //change this to hide a modal, redirect to a specific page, ect
     })
-    .catch((errors) => { //catch errors and print to console
-      console.log(errors.response.data.errors);
+    .catch((error) => { //catch errors and print to console
+      console.log(error.response.data.errors);
+      setErrors(error.response.data.errors); //display errors to user
     });
   };
 
   return (
     <div id="signup">
     <h1>Sign up!</h1>
+    <ul>
+      {errors.map((error) => (
+        <li id="error" key={error}>{error}</li>
+      ))}
+    </ul>
       <form onSubmit={handleSubmit}>
         <div>
           Name: <input name="name" type="text" className="form-control"/>
